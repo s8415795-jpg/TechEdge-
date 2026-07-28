121a9b82a4462a33001c3e91cd894f1c3d7b8159
Sneha <s8415795@gmail.com>
2026-07-28 17:40:32 +0530

    Add files via upload

---
+++ Day1(28-07-2026).cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+using namespace std;
+int main()
+
+// 10. sum of all element of array
+
+/*
+{ int a[] ={1,2,34,45,3,6,7,4,9,6};
+int i, sum=0;
+for ( i=0;i<10;i++)
+{
+	sum=sum+a[i];	
+	cout<<sum <<" ";
+}
+return 0;
+}
+*/
+
+// 9.  greatest of 3 no.
+
+
+/*
+
+{
+	int a=5;
+	int b=4;
+	int c=9;
+	if (a>b && a>c)
+	{
+		cout<<"a is greatest";
+		
+	}
+	else if (b>a && b>c)
+	{
+		cout<<"b is greatest";
+	}
+	else {
+		cout<<"c is greatest";
+	}
+}
+*/
+
+//8 swap without 3 variable
+
+
+/*
+{
+	int a=5;
+	int b=6;
+	a=b-a;
+	b=b-a;
+	a=b+a;
+	cout<<a;
+	cout<<b;
+}
+*/
+
+// 7. square of no.
+
+/*
+{
+	int num, sqr;
+	cin >> num;
+	sqr = num*num;
+	cout<< sqr ;
+}*/
+
+//6. average of 3 no.
+
+
+/*
+{
+	int a=3;
+	int b=4;
+	int h=6;
+	int avg=(a+b+h)/3;
+	cout<<avg;
+	
+}
+*/
+
+//5. volume of cube
+
+
+/*
+{
+	int a=3;
+	int b=4;
+	int h=6;
+	int volum= a*b*h;
+	cout<< volum;
+}
+*/
+
+//4. area of circle
+
+/*
+{
+	int r=5;
+	int area=3.14*r*r;
+	cout<<area;
+}*/
+
+//3. sum of 2 no
+
+
+/*
+{
+	int a=5;
+	int b=5;
+	int sum=a+b;
+	cout<<sum;
+}*/
+
+// 2.  print string by changing line
+
+
+/*{
+	cout<< "Hello "<<endl <<"TasiNCoder";
+}
+*/
+
+//1. print string in a line
+
+/*
+{
+	cout<<"Hello TasiNCoder";
+	return 0;
+}
+*/
+
+
